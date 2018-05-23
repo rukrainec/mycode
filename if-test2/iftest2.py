12d3782a830aeb5cbe01077e67ea6f98b30947c2
rukrainec <robert.ukrainec@gmail.com>
2018-05-23 14:49:04 -0400

    latest and greatest

--- if-test2/iftest2.py
+++ if-test2/iftest2.py
@@ -2,10 +2,10 @@
 #22 May 2018 Lab 12: IPv4 Testing with If
 ipchk = input('Please provide an IP address: ') #changed to prompt user for input
 
-if ipchk:
-	print('Looks like the IP address was set: ' + ipchk)
-else: #added consequences for not putting anything in
-	print('You didn\'t provide any input!')
 if ipchk == '192.168.70.1': # hypothetical default gateway address
 	print('That (' + ipchk + ') looks a lot like the default gateway, you sure about this?') # to prevent entering the default gw
+elif ipchk == '': #added consequences for not putting anything in
+	print('You didn\'t provide any input!')
+else:
+	print('Looks like the IP address was set: ' + ipchk)
 
