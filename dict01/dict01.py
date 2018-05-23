58e07774461c2b20c02e76405b465b2d75977728
rukrainec <robert.ukrainec@gmail.com>
2018-05-23 16:43:34 -0400

    lab 15, updated

--- dict01/dict01.py
+++ dict01/dict01.py
@@ -13,37 +13,33 @@ print( switch['ip'] )
 
 ## request a 'fake' key with .get() method
 print( "First test - .get()" )
-switch.get('lynx')
+print(switch.get('lynx'))
 
 print( "Second test - .get()" )
-switch.get('lynx', "THE KEY IS IN ANOTHER CASTLE!")
+print(switch.get('lynx', "THE KEY IS IN ANOTHER CASTLE!"))
 
 print( "Third test - .get()" )
-switch.get('version')
-    
-## request a 'fake' key
-# print( switch['lynx'] )
+print(switch.get('version'))
 
 print('Fourth test - .keys()')
-switch.keys()
+print(switch.keys())
 
 print('Fifth test - .values()')
-switch.values()
+print(switch.values())
 
 print( "Sixth test - .pop()" )
-switch.pop('version') # removes this key (and value) pair
-switch.keys()   # notice the value of version is gone
-switch.values() # notice the value 1.2
+print(switch.pop('version')) # removes this key (and value) pair
+print(switch.keys())   # notice the value of version is gone
+print(switch.values()) # notice the value 1.2
 
 print( "Seventh test - ADD a new value" )
 switch['adminlogin'] = 'karl08'
-switch.keys()
-switch.values()
+print(switch.keys())
+print(switch.values())
 
 print( "Eighth test - ADD a new value" )
 switch['password'] = 'qwerty'
-switch.keys()
-switch.values()
+print(switch.keys())
+print(switch.values())
     
-## request a 'fake' key
-print(switch)
+
