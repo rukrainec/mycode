0f3218241764b28e08c329fa3fc4748221ec813f
rukrainec <robert.ukrainec@gmail.com>
2018-05-23 17:59:53 -0400

    Afternoon lab done

--- loopDloop/loopsnloops.py
+++ loopDloop/loopsnloops.py
@@ -9,6 +9,6 @@
 ###########
 
 #establish dictionary
-cisco_ios = {'1)device_type': 'cisco_ios_ssh', '2)ip': '10.10.10.27', '3)username': 'admin', '4)password': 'passwd', '5)port': 22} # added int) to keys to allow for sorting
+cisco_ios = {'1)device_type': 'cisco_ios_ssh', '2)ip': '10.10.10.27', '3)username': 'admin', '4)password': 'passwd', '5)port': 22} # added #) to keys to allow for sorting
 for x in sorted(cisco_ios): # sorting dictionary for constant, predictable output
     print('CISCO LOGIN INFO - ' + str(cisco_ios[x])) # iterate dictionary, display values as strings to allow concatenation
