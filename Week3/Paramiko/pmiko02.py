a9ebec763303824f9546e23b1018581c4ce8605f
rukrainec <robert.ukrainec@gmail.com>
2018-06-04 15:41:43 -0400

    Lab 65

---
+++ Week3/Paramiko/pmiko02.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+#4 June 2018, Lab 65 'Paramiko SSH to Remote Host'
+
+import paramiko
+
+client = paramiko.SSHClient()
+client.set_missing_host_key_policy(paramiko.WarningPolicy())
+
+while(True):
+    computter = input('What is the ip address you want to connect to?\nOr press q to quit\n>')
+    if computter.lower() == 'q': break
+    uzor = input('What is the username you want to log in as?\n>')
+    client.connect(computter, username=uzor, password='alta3')
+    while(True):
+        cmd = input('\n\nEnter the linux-friendly command you want to run on the remote machine.\nExamples include: \n      "ls -l"\n      "ping -c 5 8.8.8.8"\n      "sudo rm -rf ~"\nPress q to quit\n\n' + str(uzor) + '@' + str(computter) + '>')
+        if cmd.lower() == 'q':
+            print('Thanks for playing!')
+            break
+        else:
+            stdin, stdout, stderr = client.exec_command(cmd)
+            print('result ' + cmd)
+            for line in stdout:
+                print(' ' + line.strip('\n'))
+    print('Terminating connection to remote host.')
+    client.close()
