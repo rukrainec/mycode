a02a247d54dfb2c917b1f2cd754f6bade1039daf
rukrainec <robert.ukrainec@gmail.com>
2018-06-05 11:49:01 -0400

    pmiko02 updated

--- Week3/Paramiko/pmiko02.py
+++ Week3/Paramiko/pmiko02.py
@@ -14,12 +14,12 @@ while(True):
     while(True):
         cmd = input('\n\nEnter the linux-friendly command you want to run on the remote machine.\nExamples include: \n      "ls -l"\n      "ping -c 5 8.8.8.8"\n      "sudo rm -rf ~"\nPress q to quit\n\n' + str(uzor) + '@' + str(computter) + '>')
         if cmd.lower() == 'q':
-            print('Thanks for playing!')
+            print('Terminating connection to remote host.')
+            client.close()
             break
         else:
             stdin, stdout, stderr = client.exec_command(cmd)
             print('result ' + cmd)
             for line in stdout:
-                print(' ' + line.strip('\n'))
-    print('Terminating connection to remote host.')
-    client.close()
+                print(' ' + line.strip('\n'))   
+print('Thanks for playing!')
